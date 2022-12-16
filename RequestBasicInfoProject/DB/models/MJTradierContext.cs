5f1946729d232a524b7287fcf4100340bdc1d1d4
meancl <sbe03253@naver.com>
2022-12-16 13:49:29 +0900

    프로젝트 개별 git 생성 1. MJTradier 하위 프로젝트인 DB작업용 프로젝트를 개별 git으로 생성

---
+++ RequestBasicInfoProject/DB/models/MJTradierContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RequestBasicInfoProject.DB.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestBasicInfoProject
+{
+    public class MJTradierContext : DbContext
+    {
+        public DbSet<BasicInfoReq> basicInfo { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=database-2.clmg3ftdxi2a.ap-northeast-2.rds.amazonaws.com;database=MJTradierDB;user=sbe03253;password=jin94099");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<BasicInfoReq>(entity =>
+            {
+                entity.HasKey(k => new { k.생성시간, k.종목코드});
+            });
+        }
+    }
+}
